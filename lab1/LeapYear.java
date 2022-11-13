f0c860001248a2879594e0d32f621cd9f4fc373e
lazyYNUer <884691438@qq.com>
2022-11-13 17:57:14 +0800

    Update LeapYear.java
    finish lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,8 +1,13 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author lazyYNUER
  */
 public class LeapYear {
-
+    /** determin if the given year is a leap year
+     *   @param year to be analyzed
+     */
+    public static boolean isLeapYear(int year) {
+        return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+    }
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
